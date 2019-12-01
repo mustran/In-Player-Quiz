3297c1530740e4b7c18b349a7a47644b2362055d
mustran <ademimusli000@gmail.com>
2019-12-01 13:32:08 +0100

    save and display high scores from local storage

---
+++ scripts/saveScore.js
@@ -0,0 +1,43 @@
+const username = document.querySelector("#username");
+const submitScore = document.querySelector("#submitScore");
+const scores = document.querySelector("#scores");
+
+let allUsers = JSON.parse(localStorage.getItem("users"));
+
+if (allUsers == null) {
+    localStorage.setItem("users", JSON.stringify([]));
+    allUsers = [];
+}
+
+let userAndScore = {
+    username: null,
+    score: localStorage.getItem("userScore")
+};
+
+submitScore.addEventListener("click", event => {
+    userAndScore.username = username.value;
+
+    allUsers.push(userAndScore);
+    localStorage.setItem("users", JSON.stringify(allUsers));
+});
+
+let displayScores;
+let allScores = JSON.parse(localStorage.getItem("users")).sort((a, b) => b.score - a.score);
+if (allScores.length == 0) {
+    displayScores = `<h1>The are no scores</h1>`;
+} else {
+    displayScores = `
+    <ul class="list-group">
+    ${allScores
+        .map(
+            (user, index) =>
+                `<li  class="list-group-item d-flex justify-content-between align-items-center">${index +
+                    1}. ${user.username} <span class="badge badge-primary badge-pill">${
+                    user.score
+                }</span></li>`
+        )
+        .join(" ")}
+    </ul>
+    `;
+}
+scores.innerHTML = displayScores;
