ae39af92a970d3532fea62651fa4ae605d63507f
mustran <ademimusli000@gmail.com>
2019-12-01 12:56:45 +0100

    display feedback for correct/incorrect answers

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,4 +1,4 @@
-const choiceOptions = document.querySelector(".choice");
+const choiceOptions = document.querySelectorAll(".choice");
 const questionText = document.querySelector("#questionText");
 const countQuestionsTag = document.querySelector("#countQuestions");
 
@@ -15,6 +15,9 @@ getDataset().then(data => {
     generateRandomQuestions(questions);
 });
 
+// we need the correct answer after we randomly pick a question
+let correctAnswer;
+
 generateRandomQuestions = questions => {
     let questionsLength = questions.length;
 
@@ -22,5 +25,41 @@ generateRandomQuestions = questions => {
     const index = Math.floor(Math.random() * questionsLength);
     const questionPickedRandomly = questions[index];
 
+    //question section
     questionText.innerHTML = questionPickedRandomly.question;
+
+    //display question options
+    choiceOptions.forEach((choice, index) => {
+        choice.innerHTML = questionPickedRandomly.available_choices[index];
+    });
+
+    correctAnswer = questionPickedRandomly.correct_answer;
+
+    //remove the picked question
+    questions.splice(index, 1);
+    console.log(questions);
 };
+
+choiceOptions.forEach(el => {
+    el.addEventListener("click", event => {
+        let elementTag = event.target;
+        let selectedChoice = event.srcElement.innerText;
+
+        //animate red if the selected choice is wrong
+        if (selectedChoice !== correctAnswer) {
+            elementTag.classList.add("btn-danger");
+            setTimeout(() => {
+                generateRandomQuestions(questions);
+                elementTag.classList.remove("btn-danger");
+            }, 800);
+        }
+        //animate green color for the correct answer
+        else {
+            elementTag.classList.add("btn-success");
+            setTimeout(() => {
+                generateRandomQuestions(questions);
+                elementTag.classList.remove("btn-success");
+            }, 800);
+        }
+    });
+});
