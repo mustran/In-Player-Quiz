99508b2b5671d053ce0910f3a9b1bd064ca76390
mustran <ademimusli000@gmail.com>
2019-12-01 15:02:14 +0100

    highscores saving empty users bug fixed

--- scripts/saveScore.js
+++ scripts/saveScore.js
@@ -14,30 +14,41 @@ let userAndScore = {
     score: localStorage.getItem("userScore")
 };
 
+displayScoresSorted();
+
 submitScore.addEventListener("click", event => {
+    // event.preventDefault();
     userAndScore.username = username.value;
 
-    allUsers.push(userAndScore);
-    localStorage.setItem("users", JSON.stringify(allUsers));
+    //store the username if it is not empty
+    if (username.value !== "") {
+        allUsers.push(userAndScore);
+        localStorage.setItem("users", JSON.stringify(allUsers));
+        username.disabled = true;
+        username.value = "";
+    }
+    displayScoresSorted();
 });
 
-let displayScores;
-let allScores = JSON.parse(localStorage.getItem("users")).sort((a, b) => b.score - a.score);
-if (allScores.length == 0) {
-    displayScores = `<h1>The are no scores</h1>`;
-} else {
-    displayScores = `
-    <ul class="list-group">
-    ${allScores
-        .map(
-            (user, index) =>
-                `<li  class="list-group-item d-flex justify-content-between align-items-center">${index +
-                    1}. ${user.username} <span class="badge badge-primary badge-pill">${
-                    user.score
-                }</span></li>`
-        )
-        .join(" ")}
-    </ul>
-    `;
+function displayScoresSorted() {
+    let displayScores;
+    let allScores = JSON.parse(localStorage.getItem("users")).sort((a, b) => b.score - a.score);
+    if (allScores.length == 0) {
+        displayScores = `<h1>The are no scores</h1>`;
+    } else {
+        displayScores = `
+        <ul class="list-group">
+        ${allScores
+            .map(
+                (user, index) =>
+                    `<li  class="list-group-item d-flex justify-content-between align-items-center">${index +
+                        1}. ${user.username} <span class="badge badge-primary badge-pill">${
+                        user.score
+                    }</span></li>`
+            )
+            .join(" ")}
+        </ul>
+        `;
+    }
+    scores.innerHTML = displayScores;
 }
-scores.innerHTML = displayScores;
